f9af4ce693e6ce8182c884ab7aa56eab8eb35069
sultanrhinosalfriandry <sultanrhinosalfriandry@gmail.com>
2022-10-31 17:14:19 +0700

    snackbar

---
+++ notip_wijet/android/app/src/main/kotlin/com/example/notip_wijet/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.notip_wijet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ notip_wijet/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Notip Wijet")),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              MaterialButton(child: Text("Show Toast"), onPressed: showToast),
+              const SnackBarPage(),
+            ],
+          ) ),
+         ),
+    );
+  }
+
+  void showToast(){
+    setState(() {
+      Fluttertoast.showToast(
+        msg: "This is toast notification",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM);
+  });
+    }
+  }
+  class SnackBarPage extends StatelessWidget {
+  const SnackBarPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          final snackBar = SnackBar(
+            content: const Text('Yay! A SnackBar!'),
+            action: SnackBarAction(
+              label: 'Undo',
+              onPressed: () {
+                // Some code to undo the change.
+              },
+            ),
+          );
+
+          // Find the ScaffoldMessenger in the widget tree
+          // and use it to show a SnackBar.
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        },
+        child: const Text('Show SnackBar'),
+      ),
+    );
+  }
+}
